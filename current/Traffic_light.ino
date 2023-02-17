a774479ed40953e5cf08b75bbde38b0d44a5ce51
BogdanGorelkin <b.gorelkin@yandex.ru>
2023-02-18 00:18:28 +0100

    interrupt version

--- current/Traffic_light.ino
+++ current/Traffic_light.ino
@@ -1,45 +1,93 @@
 //preparing stuff for next lesson
+const int BTN = D4;
+const int RED = D7;
+const int YELLOW = D6;
+const int GREEN = D5;
+bool interrupt_flag = false;
 
-#define BTN D4
-#define RED D3
-#define YELLOW D2
-#define GREEN D1
+ICACHE_RAM_ATTR void btn_pressed() {
+  Serial.println("btn pressed!");
+  interrupt_flag = true;
+}
+
+void start_traffic_light() {
+  Serial.println("Start traffic light");
+  digitalWrite(RED, LOW);
+  digitalWrite(YELLOW, LOW);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(RED, HIGH); //Turn on red
+  delay(4000);  // wait 4 sec
+
+  digitalWrite(YELLOW, HIGH);
+  delay(1000);
+
+  digitalWrite(YELLOW, LOW); // Turn off yellow
+  digitalWrite(RED, LOW); // Turn off red
+
+  digitalWrite(GREEN, HIGH);
+  delay(5000);
+  
+  // Start blinking
+  digitalWrite(GREEN, LOW);
+  delay(500);
+  digitalWrite(GREEN, HIGH);
+  delay(500);
+  digitalWrite(GREEN, LOW);
+  delay(500);
+  digitalWrite(GREEN, HIGH);
+  delay(500);
+  digitalWrite(GREEN, LOW);
 
 
-int currentState;
-unsigned long previousMillis = 0;
-const long interval = 1000;
-int ledState = LOW;
+  digitalWrite(YELLOW, HIGH);
+  delay(2000);
+  Serial.println("End traffic light");
+}
 
 void setup() {
   pinMode(RED, OUTPUT);
   pinMode(YELLOW, OUTPUT); 
   pinMode(GREEN, OUTPUT); 
   pinMode(BTN, INPUT_PULLUP);
-  
+  attachInterrupt(digitalPinToInterrupt(BTN), btn_pressed, RISING);
   Serial.begin(115200);
 }
 
 
 void loop() {
-  unsigned long currentMillis = millis();
+  while(!interrupt_flag){
+    Serial.println("Start traffic light");
+    digitalWrite(RED, LOW);
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(RED, HIGH); //Turn on red
+    delay(4000);  // wait 4 sec
 
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
+    digitalWrite(YELLOW, HIGH);
+    delay(1000);
 
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
+    digitalWrite(YELLOW, LOW); // Turn off yellow
+    digitalWrite(RED, LOW); // Turn off red
 
-    digitalWrite(RED, ledState);   
-    digitalWrite(YELLOW, ledState);
-    digitalWrite(GREEN, ledState);
+    digitalWrite(GREEN, HIGH);
+    delay(5000);
+    
+    // Start blinking
+    digitalWrite(GREEN, LOW);
+    delay(500);
+    digitalWrite(GREEN, HIGH);
+    delay(500);
+    digitalWrite(GREEN, LOW);
+    delay(500);
+    digitalWrite(GREEN, HIGH);
+    delay(500);
+    digitalWrite(GREEN, LOW);
+
+
+    digitalWrite(YELLOW, HIGH);
+    delay(2000);
+    Serial.println("End traffic light");
   }
-  
-  currentState = digitalRead(BTN);
-  Serial.println(currentState);
+  Serial.println("set flag to false");
+  interrupt_flag = false;
 }
